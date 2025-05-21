5bbb0567770589836caef8f92cca389bec5bc1c3
Durosinlohun Uthman <udurosinlohun@gmail.com>
2025-05-21 10:18:35 +0100

    modified performance.sql file to include the Explain statement and added where clause in the initial  query

--- database-adv-script/perfomance.sql
+++ database-adv-script/perfomance.sql
@@ -15,4 +15,25 @@ SELECT
 FROM Booking b 
 JOIN User u ON b.user_id = u.user_id 
 JOIN Property p ON b.property_id = p.property_id 
-JOIN Payment pay ON b.booking_id = pay.booking_id;
+JOIN Payment pay ON b.booking_id = pay.booking_id
+WHERE b.start_date BETWEEN '2025-01-01' AND '2025-12-31';
+
+
+EXPLAIN
+SELECT     
+    b.booking_id,    
+    b.start_date,
+    u.user_id,    
+    u.first_name AS user_name,    
+    u.email AS user_email,  
+    p.property_id,    
+    p.name AS property_name,    
+    p.location,
+    pay.payment_id,    
+    pay.amount,    
+    pay.payment_date 
+FROM Booking b 
+JOIN User u ON b.user_id = u.user_id 
+JOIN Property p ON b.property_id = p.property_id 
+JOIN Payment pay ON b.booking_id = pay.booking_id
+WHERE b.start_date BETWEEN '2025-01-01' AND '2025-12-31';
